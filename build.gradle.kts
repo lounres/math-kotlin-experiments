90b1cdb44f9c3eb1026d4dd849ff50a5026f4f27
Gleb Minaev <43728100+lounres@users.noreply.github.com>
2022-08-22 18:51:17 +0300

    Add Gradle.

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
+
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    with(libs.plugins) {
+        alias(kotlin.jvm) apply false
+    }
+}
+
+allprojects {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        maven("https://repo.kotlin.link")
+//        maven("https://oss.sonatype.org/content/repositories/snapshots")
+    }
+}
+
+subprojects {
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    configure<KotlinJvmProjectExtension> {
+        target.compilations.all {
+            kotlinOptions {
+                freeCompilerArgs += listOf(
+                    "-Xcontext-receivers",
+//                    "-Xuse-k2",
+                )
+                jvmTarget = properties["jvmTarget"] as String
+            }
+        }
+
+        sourceSets {
+            all {
+                languageSettings {
+                    progressiveMode = true
+                }
+            }
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+rootProject.name = "math-kotlin-experiments"
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://repo.kotlin.link")
+    }
+}
+
+include(
+    "polynomialParametrisationIsAlgebraic",
+    "voronoiDiagram",
+    "kosholka",
+)
